4e63925f4149506da40174d692ee8f28805d8c43
Rafael Morais <rafaelm.morais@hotmail.com>
2024-09-12 11:09:04 -0300

    exercicios modulo 21 atualizados

--- Exercicio_M20_3.js
+++ Exercicio_M20_3.js
@@ -2,17 +2,15 @@
 //ficamos com 3, 5, 6 e 9. A soma desses múltiplos é 23. Crie uma função que retorne 
 //a soma de todos os múltiplos de 5 ou 7 abaixo de 1000.
 
-let mult5 = 0
-let mult7 = 0
-somar(1000);
-function somar(limite){
-    for(i = 0; i <= limite; i++)
-    {
-        if(i % 5 === 0){mult5 += i}
-        if(i % 7 === 0){mult7 += i}
+function somar(limite) {
+    let mult5 = 0;
+    let mult7 = 0;
+
+    for (let i = 0; i <= limite; i++) {
+        if (i % 5 === 0) { mult5 += i; }
+        if (i % 7 === 0) { mult7 += i; }
     }
+    return mult5 + mult7;
 }
 
-console.log(mult5 + mult7)
-
-module.exports = { somar, mult5, mult7 }
+module.exports = { somar };
--- Exercicio_M20_3.test.js
+++ Exercicio_M20_3.test.js
@@ -1,8 +1,9 @@
-const { somar, mult5, mult7 } = require('./Exercicio_M20_3')
+const { somar } = require('./Exercicio_M20_3')
 
 describe('Teste Exercicio 3', () => {
-    it('Deve apresentar a soma de todos os numeros multiplos de 5 e 7', () => {
-        expect(mult5 + mult7).toBe(171571)
+    it('Deve apresentar a soma de todos os numeros multiplos de 5 e 7 até 1000', () => {
+        const res = somar(1000)
+        expect(res).toBe(171571)
     });
 })
 
